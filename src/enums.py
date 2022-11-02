ffe599a2ac50fa94bb6af06f1df1b78cfabe9808
GavrilinEugene <julentver1@gmail.com>
2022-11-02 18:57:34 +0300

    enums

---
+++ src/enums.py
@@ -0,0 +1,14 @@
+# insert mapbox token (free)
+token = 'pk.*******'
+
+# insert gis token (not free)
+key = ''
+
+# h3 index resolution
+RES = 10
+
+postgress_connection = ''
+postgress_db = ''
+lp = ''
+schema = ''
+
